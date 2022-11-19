ff356707b1288aa59b1fc1466d85046236297c59
Новиков Илья <nov.ilucha@yandex.ru>
2022-11-19 18:40:08 +0300

    теперь папка со схемами создаётся сама при установке

--- setup_docker.sh
+++ setup_docker.sh
@@ -4,6 +4,15 @@ sudo systemctl start docker
 # build image
 sudo docker build -t core .
 # run image
+# создаю папку для двухсторонней связи
+# с контейнером
+dir = ~/core_schema
+if [[ ! -e $dir ]]; then
+    mkdir $dir
+elif [[ ! -d $dir ]]; then
+    echo "$dir already exists but is not a directory" 1>&2
+fi
+
 sudo docker run -itd --name core -e DISPLAY \
 	-v /tmp/.X11-unix:/tmp/.X11-unix:rw \
 	-v ~/core_schema:/root/core_schema:rw \
